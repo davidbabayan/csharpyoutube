1c3ced3687a354ef356e27cc1d645cf0780bfc48
David Babayan <me@davidbabayan.com>
2021-12-25 19:02:21 +0400

    Lecture 3 added

---
+++ Lecture 3/Homework/Program.cs
@@ -0,0 +1,43 @@
+﻿// Excersise 1
+// Console.Write("Enter your name: ");
+// string name = Console.ReadLine();
+// Console.Write("Enter your age: ");
+// string age = Console.ReadLine();
+// Console.WriteLine($"Hello {name}! You are {age} year old!");
+
+// Excersise 2
+// int a, b, c;
+
+// Console.Write("Enter parameter A: ");
+// a = int.Parse(Console.ReadLine());
+// Console.Write("Enter parameter B: ");
+// b = int.Parse(Console.ReadLine());
+// Console.Write("Enter parameter C: ");
+// c = int.Parse(Console.ReadLine());
+
+// double x1 = 0 - b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+// double x2 = 0 - b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+
+// Console.WriteLine($"For your equision the x1 is {x1} and x2 is {x2}");
+
+// Excsersise 3
+// string word;
+// Console.Write("Enter any english word: ");
+// word = Console.ReadLine();
+// string newWord = word
+//     .ToLower()
+//     .Replace("a", "")
+//     .Replace("e", "")
+//     .Replace("i", "")
+//     .Replace("o", "")
+//     .Replace("u", "")
+//     .Replace("y", "");
+// Console.WriteLine(newWord);
+
+// Excersise 4
+// int birthYear;
+
+// Console.Write("Enter your birth year: ");
+// birthYear = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"You are {2021 - birthYear} year old!");
---
+++ Lecture 3/Variables/Program.cs
@@ -0,0 +1,49 @@
+﻿// Փոփոխականներ
+
+// int i = 0; // ամբողջ թվեր
+// double d = 12.1; float f = 15.6f; // կոտորակային թվեր
+// char c = 'a'; // սիմվոլներ
+// bool b = false; // տրամաբանական տիպ կարող է լինել true կամ false
+// string s = "some text"; // տեքստային տիպ
+
+// var num = 13;
+
+// Console.Write("Enter your name: ");
+// string name = Console.ReadLine();
+// Console.Write("Enter your age: ");
+// string age = Console.ReadLine();
+
+// Console.WriteLine("Hello " + name + "! You are " + age + " years old!");
+
+// int i = int.Parse("12");
+// Console.WriteLine(i);
+// string s = (true).ToString();
+// Console.WriteLine(s);
+// int i = Convert.ToInt32(false);
+// Console.WriteLine(i);
+// bool b = Convert.ToBoolean(0);
+// Console.WriteLine(b);
+
+// + - * / %
+// System.Console.WriteLine(20 % 3);
+// System.Console.WriteLine("ab"+(12+5));
+
+// += -= *= /=
+// ++ --
+// int i = 5;
+// int num1 = i++;
+// System.Console.WriteLine(i);
+// System.Console.WriteLine(num1);
+
+// Console.Write("Enter a number: ");
+// double num = double.Parse(Console.ReadLine());
+
+// Console.WriteLine("Absalut value: " + Math.Abs(num));
+// Console.WriteLine("Rounded value: " + Math.Round(num));
+// Console.WriteLine("Ceiled value: " + Math.Ceiling(num));
+// Console.WriteLine("Floored value: " + Math.Floor(num));
+// Console.WriteLine("Is num is greater or 15: " + Math.Max(15, num));
+// Console.WriteLine(Math.Min(15, num));
+// Console.WriteLine(Math.Pow(num, 2));
+// Console.WriteLine(Math.Sqrt(num));
+// Console.WriteLine(Math.Cbrt(num));
